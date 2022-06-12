97a9736c8017c9a1be6692d961d1f598aa5309fc
Felipe Matheus <felipe774@gmail.com>
2022-06-12 11:55:40 -0300

    Criação pastas dos components

---
+++ Front/src/components/types/index.ts
@@ -0,0 +1,12 @@
+export interface UserLogin {
+    
+}
+export interface User {
+    id: number;
+    nome: string;
+    email: string;
+    senha: string;
+    confirmarSenha: string;
+    ap: string;
+    imagem: string;
+}
