b38308fb4b9b4726d4b970c8476b9c41e6f19895
Felipe Matheus <felipe774@gmail.com>
2022-06-13 20:25:13 -0300

    Criação do pages

---
+++ Front/src/pages/Home/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Container from "../../components/Container";
+import FormRegister from "../../components/FormRegister";
+
+const Home: React.FC = () => {
+    return (
+        <Container>
+        <FormRegister />
+        </Container>
+    );
+};
+
+export default Home;
