15e2d557bb73e7af89fb34ab0654913248bd0b09
Laura Neves <lauratneves@gmail.com>
2022-06-13 16:27:10 -0300

    criando arquivos de migrations e seeders

---
+++ Back/src/infrastructure/database/seeders/20220613122507-users.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const { faker } = require("@faker-js/faker");
+// import bcrypt from "bcryptjs";
+
+// const senha = bcrypt.hashSync("123456", 10);
+
+let seed = []
+
+for(let i=0; i<10; i++) {
+  seed.push(
+    {
+      name: faker.name.findName(),
+      email: faker.internet.email(),
+      appartment: faker.random.numeric(4),
+      password: faker.internet.password(),
+      status: faker.datatype.boolean(),
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }
+  )
+}
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert("users", seed);
+  },
+
+  async down (queryInterface, Sequelize) {
+
+  }
+};
---
+++ Back/src/infrastructure/database/seeders/20220613122514-posts.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const { faker } = require("@faker-js/faker");
+
+let seed = []
+
+for(let i=1; i<=20; i++) {
+  seed.push(
+    {
+      content: faker.random.alpha(100),
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      user_id: i,
+    }
+  )
+}
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert("posts", seed);
+  },
+
+  async down (queryInterface, Sequelize) {
+    
+  }
+};
