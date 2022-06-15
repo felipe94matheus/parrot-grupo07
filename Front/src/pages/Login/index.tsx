75f5f36aa4f2eafe9f2dc51e81e5895fd7a94873
Felipe Matheus <felipe774@gmail.com>
2022-06-15 00:06:20 -0300

    criação da page login

---
+++ Front/src/pages/Login/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Container from "../../components/Container";
+import FormLogin from "../../components/FormLogin";
+
+const Login: React.FC = () => {
+    return (
+        <Container>
+            <FormLogin />
+        </Container>
+    );
+};
+
+export default Login;
